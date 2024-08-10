d7bad0977d427e4af2bae10cdbb295c688f99be4
sairamthammaneni <166827331+sairamthammaneni@users.noreply.github.com>
2024-08-10 12:17:21 +0530

    Create default.java

---
+++ default.java
@@ -0,0 +1,20 @@
+public class default{
+	static int INT;
+	static float FLOAT;
+	static double DOUBLE;
+	static boolean BOOLEAN;
+	static char CHAR;
+	static short SHORT;
+	static long LONG;
+	static byte BYTE;
+	public static void main(String[] args){
+		System.out.println("int="+INT);
+		System.out.println("float="+FLOAT);
+		System.out.println("double="+DOUBLE);
+		System.out.println("boolean="+BOOLEAN);
+		System.out.println("char="+CHAR);
+		System.out.println("short="+SHORT);
+		System.out.println("long="+LONG);
+		System.out.println("byte="+BYTE);
+	}
+}
